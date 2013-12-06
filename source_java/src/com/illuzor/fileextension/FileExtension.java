6023e05d9333045e77a87028a4dced76a88a7d20
illuzor <illuzor@gmail.com>
2013-12-06 22:05:00 +0400

    added build.xml

--- source_java/src/com/illuzor/fileextension/FileExtension.java
+++ source_java/src/com/illuzor/fileextension/FileExtension.java
@@ -4,25 +4,25 @@ import com.adobe.fre.FREContext;
 import com.adobe.fre.FREExtension;
 
 public class FileExtension implements FREExtension {
-	
-	FileExtensionContext context;
+        
+        FileExtensionContext context;
 
-	@Override
-	public FREContext createContext(String arg0) {
-		context = new FileExtensionContext();
-		return context;
-	}
+        @Override
+        public FREContext createContext(String arg0) {
+                context = new FileExtensionContext();
+                return context;
+        }
 
-	@Override
-	public void dispose() {
-		context.dispose();
-		context = null;
-	}
+        @Override
+        public void dispose() {
+                context.dispose();
+                context = null;
+        }
 
-	@Override
-	public void initialize() {
-		// TODO Auto-generated method stub
+        @Override
+        public void initialize() {
+                // TODO Auto-generated method stub
 
-	}
+        }
 
 }
--- source_java/src/com/illuzor/fileextension/FileExtensionContext.java
+++ source_java/src/com/illuzor/fileextension/FileExtensionContext.java
@@ -9,17 +9,16 @@ import com.illuzor.fileextension.functions.OpenFileFuntion;
 
 public class FileExtensionContext extends FREContext {
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
+        @Override
+        public void dispose() {
+                // TODO Auto-generated method stub
+        }
 
-	}
-
-	@Override
-	public Map<String, FREFunction> getFunctions() {
-		Map<String, FREFunction> map = new HashMap<String, FREFunction>();
-		map.put("openFile", new OpenFileFuntion());
-		return map;
-	}
+        @Override
+        public Map<String, FREFunction> getFunctions() {
+                Map<String, FREFunction> map = new HashMap<String, FREFunction>();
+                map.put("openFile", new OpenFileFuntion());
+                return map;
+        }
 
 }
